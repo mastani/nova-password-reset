70d5f8e8cf712d46aa7e6251276ef26cf53fe60c
Newton Gama Junior <newton@galileosoft.com.br>
2020-12-12 23:17:53 -0300

    - New publishing for config and translations - Use of vue-simple-password-meter - Calling method to password minimal size

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-        <heading class="mb-6">Reset My Password</heading>
+        <heading class="mb-6">{{__('Reset My Password')}}</heading>
 
         <div class="card">
             <div v-if="errors.length" class="flex border-40">
                 <div class="px-8 py-6">
                     <p>
-                        <b>Please correct the following error(s):</b>
-                    <ul>
-                        <li v-for="error in errors" style="padding-top: 10px; color: #ff1515">{{ error }}</li>
-                    </ul>
+                        <b>{{ __('Please correct the following error(s):')}}</b>
+                        <ul>
+                            <li v-for="error in errors" style="padding-top: 10px; color: #ff1515">{{ error }}</li>
+                        </ul>
                     </p>
                 </div>
             </div>
@@ -17,7 +17,7 @@
             <div class="flex border-40">
                 <div class="w-1/5 px-8 py-6">
                     <label for="old_password" class="inline-block text-80 pt-2 leading-tight">
-                        Current password
+                      {{ __('Current password') }}
                         <span class="text-danger text-sm">*</span>
                     </label>
                 </div>
@@ -33,7 +33,7 @@
             <div class="flex border-40">
                 <div class="w-1/5 px-8 py-6">
                     <label for="new_password" class="inline-block text-80 pt-2 leading-tight">
-                        New password
+                      {{ __('New password') }}
                         <span class="text-danger text-sm">*</span>
                     </label>
                 </div>
@@ -43,13 +43,14 @@
                         id="new_password" type="password"
                         class="w-full form-control form-input form-input-bordered"
                         name="new_password">
+                  <password-meter :password="new_password" />
                 </div>
             </div>
 
             <div class="flex border-40">
                 <div class="w-1/5 px-8 py-6">
                     <label for="confirm_password" class="inline-block text-80 pt-2 leading-tight">
-                        Confirm password
+                      {{ __('Confirm password') }}
                         <span class="text-danger text-sm">*</span>
                     </label>
                 </div>
@@ -59,6 +60,7 @@
                         id="confirm_password" type="password"
                         class="w-full form-control form-input form-input-bordered"
                         name="confirm_new_password">
+                  <password-meter :password="confirm_new_password" />
                 </div>
             </div>
 
@@ -66,7 +68,7 @@
                 <div class="px-8 py-6">
                     <button @click="submitForm()" type="submit"
                             class="btn btn-default btn-primary inline-flex items-center relative">
-                        <span>Save Password</span>
+                        <span>{{ __('Save Password') }}</span>
                     </button>
                 </div>
             </div>
@@ -75,40 +77,49 @@
 </template>
 
 <script>
+    import passwordMeter from "vue-simple-password-meter";
     export default {
+        components: { passwordMeter },
         data() {
             return {
                 errors: [],
                 current_password: null,
                 new_password: null,
-                confirm_new_password: null
+                confirm_new_password: null,
+                min_password_size: null
             }
         },
         mounted() {
-            //
+            this.getPasswordSize()
         },
         methods: {
+            getPasswordSize: function () {
+                Nova.request().get('/nova-vendor/nova-password-reset/min-password-size').then(response => {
+                        this.min_password_size = response.data.minpassw;
+                    }
+                );
+            },
             checkForm: function () {
                 this.errors = [];
 
                 if (!this.current_password) {
-                    this.errors.push('Current password required.');
+                    this.errors.push(this.__('novaPasswordReset.oldPasswordRequired'));
                 }
 
                 if (!this.new_password) {
-                    this.errors.push('New password required.');
+                    this.errors.push(this.__('novaPasswordReset.newPasswordRequired'));
                 }
 
-                if (this.new_password && this.new_password.length < 5) {
-                    this.errors.push('New password must be greater than or equal 5 characters.');
+                if (this.new_password && this.new_password.length < this.min_password_size) {
+                    this.errors.push(this.__('novaPasswordReset.newPasswordSize',{ minpass: this.min_password_size }));
                 }
 
                 if (!this.confirm_new_password) {
-                    this.errors.push('New password confirmation required.');
+                    this.errors.push(this.__('novaPasswordReset.confirmPasswordRequired'));
                 }
 
-                if (this.confirm_new_password && this.confirm_new_password.length < 5) {
-                    this.errors.push('New password confirmation must be greater than or equal 5 characters.');
+                if (this.confirm_new_password && this.confirm_new_password.length < this.min_password_size) {
+                    this.errors.push(this.__('novaPasswordReset.confirmPasswordSize', { minpass: this.min_password_size }));
                 }
             },
             submitForm: function () {
@@ -122,7 +133,7 @@
                     formData.append('new_password', this.new_password)
                     formData.append('confirm_new_password', this.confirm_new_password)
                 })).then(_ => {
-                    this.$toasted.show('Password updated successfully.', {type: 'success'})
+                    this.$toasted.show(this.__('novaPasswordReset.success'), {type: 'success'})
                 }).catch(reason => {
                     if (reason.response.data && reason.response.data.errors) {
                         let errors = reason.response.data.errors;
