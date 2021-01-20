68cad2f3fa0eb1768ded170806f55e1d62cc50bd
tiagoraposeiraff <tiagor@futurefuel.io>
2021-01-20 17:57:07 +0000

    adding customPasswordRule to config

--- config/nova-password-reset.php
+++ config/nova-password-reset.php
@@ -11,6 +11,19 @@ return [
     |
     */
 
-    'min_password_size' => 5,
+    'min_password_size' => 10,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Password Rule
+    |--------------------------------------------------------------------------
+    |
+    | Determine a custom password rule to be used
+    | Any of the laravel existing validations can be used or your custom one
+    | Default is `string`.
+    |
+    */
+
+    'custom_password_rule' => 'string'
 
 ];
